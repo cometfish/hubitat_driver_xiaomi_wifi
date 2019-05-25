021f36aba944cab79753efb291c79109c0e806a6
cometfish <me+git@cporter.dev>
2019-05-25 14:45:49 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Xiaomi WiFi driver
+
+Hubitat Driver for controlling Xiaomi WiFi devices locally, with no hub or cloud connection required.
+Requires RPi set up to do the AES-128-CBC encrypt/decrypt.
+
+Tested on:
+* chuangmi.plug.m1
+
+1. Add `plug_m1.groovy` to your Hubitat as a new Driver (under `Drivers Code`)
+2. Add a new device for your WiFi Socket to your Hubitat, set device Type to your User driver of 'Xiaomi WiFi Socket'
+3. Assign your plug a static IP, and enter the IP into the device's `IP Address` setting in Hubitat.
+4. (Optional) If your device is linked to the Mi Cloud, it may be hiding its device token from all other requests, including this driver - if so, manually enter the token into the `Device Token` setting in Hubitat. Try the options here to obtain your token if you don't already know it - otherwise you may need to reset the device: https://github.com/jghaanstra/com.xiaomi-miio/blob/master/docs/obtain_token.md
+5. Configure your RPi:
+    1. Make sure `openssl`, `sed` and `xxd` are installed, as well as a web server (eg. `apache`)
+    2. Copy `aes.cgi` to the `cgi-bin` folder of your web server's root dir
+    3. Assign your RPi a static IP, and enter the IP into the device's `AES script IP` setting in Hubitat
+6. Pressing Refresh on the device in Hubitat should now load the current switch state (on/off) and the temperature reading. Enjoy :)
+
+References:
+Mi Home Binary Protocol: https://github.com/OpenMiHome/mihome-binary-protocol
+Working protocol example: https://github.com/aholstenson/miio
---
+++ plug_m1.groovy
@@ -0,0 +1,232 @@
+/*
+ * Xiaomi WiFi Socket driver (chuangmi.plug.m1)
+ *
+ * Controls the original Xiaomi WiFi socket directly via local commands (no hub or cloud needed)
+ * Requires RPi set up to do the AES-128-CBC encrypt/decrypt (see included bash script)
+ * 
+ */
+
+metadata {
+    definition(name: "Xiaomi WiFi Socket", namespace: "community", author: "cometfish") {
+        capability "Actuator"
+        capability "Switch"
+        capability "Sensor"
+		capability "TemperatureMeasurement"
+		
+		attribute "temperature", "number"
+		
+		command "refresh"
+    }
+}
+
+preferences {
+    section("URIs") {
+        input "ipAddress", "text", title: "IP Address", required: true
+		input "token", "text", title: "Device Token (optional if device is uninitialised)", required: false
+		input "aesIP", "text", title: "AES script IP", required: true
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+    }
+}
+
+import java.security.MessageDigest
+
+def updated() {
+	if (settings.token!="") {
+		state.token = settings.token
+	} 
+}
+def refresh() {
+	connect(tokenResponseStatus)
+}
+def sendMsgStatus(rdeviceID, rtoken, rstamp) {
+	sendMsg('{"id": 1, "method": "get_prop", "params": ["power", "temperature"]}', rdeviceID, rtoken, rstamp)
+}
+def on() {
+	connect(tokenResponseOn)
+}
+def sendMsgOn(rdeviceID, rtoken, rstamp) {
+	
+	sendMsg('{"id": 2, "method": "set_power", "params": ["on"]}', rdeviceID, rtoken, rstamp) 
+}
+def off() {
+	connect(tokenResponseOff)
+}
+def sendMsgOff(rdeviceID, rtoken, rstamp) {
+	sendMsg('{"id": 3, "method": "set_power", "params": ["off"]}', rdeviceID, rtoken, rstamp)
+}
+
+def parse(String description) {
+	log.info parseLanMessage(description).payload
+}
+def tokenResponseStatus(String description) {
+	tokenResponse(description, "status")			
+}
+def tokenResponseOn(String description) {
+	tokenResponse(description, "on")
+}
+def tokenResponseOff(String description) {
+	tokenResponse(description, "off")
+} 
+def tokenResponse(String description, String sendMsg) {
+	if (logEnable) log.info 'hello received!' 
+	resp=parseLanMessage(description).payload
+	if (logEnable) log.info resp
+	
+	rdeviceID = resp.substring(16, 24)
+	state.deviceID = rdeviceID
+	if (logEnable) log.info "DeviceID: " + resp.substring(16,24)
+	
+	rstamp = hubitat.helper.HexUtils.hexStringToInt(resp.substring(24,32))
+	state.stamp=rstamp
+	if (logEnable) log.info "Stamp:" +rstamp
+	
+	rtoken =settings.token
+	if (rtoken == null || rtoken =="") {
+		rtoken=resp.substring(32,64)
+		state.token=rtoken
+		if (logEnable) log.info "Token: " + resp.substring(32,64)
+	}
+	
+	switch (sendMsg) {
+		case "status":
+		sendMsgStatus(rdeviceID, rtoken, rstamp)
+		break
+		case "on":
+		sendMsgOn(rdeviceID, rtoken, rstamp)
+		break
+		case "off":
+		sendMsgOff(rdeviceID, rtoken, rstamp)
+		break
+	}
+} 
+def msgResponse(String description) {
+	if (logEnable) log.info 'message received!!!'
+	resp=parseLanMessage(description).payload
+	if (logEnable) log.info resp
+	decrypted = aesDecrypt(resp.substring(64,resp.length()))
+	if (logEnable) log.info decrypted
+	json = new groovy.json.JsonSlurper().parseText(decrypted)
+	if (json.error!=null) {
+	log.error 'Error returned:' +json.error.message
+		return
+	} 
+	switch (json.id) {
+		case 1: //status
+			sendEvent(name: "switch", value: json.result[0], isStateChange: true)
+			sendEvent(name: "temperature", value: json.result[1], isStateChange: true)
+			break
+		case 2: //on
+			if (json.result[0]=='ok') {
+				sendEvent(name: "switch", value: "on", isStateChange: true)
+			} else {
+				log.error 'Unknown response received:' +decrypted
+			} 
+		break
+		case 3: //off
+			if (json.result[0]=='ok') {
+				sendEvent(name: "switch", value: "off", isStateChange: true)
+			} else {
+				log.error 'Unknown response received:' +decrypted
+			} 
+		break
+		default:
+			log.error 'Unknown response received' +json.id
+		break
+	} 
+} 
+def sendMsg(msg, rdeviceID, rtoken, rstamp) {
+	if (logEnable) log.info 'sendMsg'
+	msgstr=aesEncrypt(msg)
+	if (logEnable) log.info msgstr
+	rstamp = rstamp+1
+	state.stamp = rstamp
+	stampstr = hubitat.helper.HexUtils.integerToHexString(rstamp,4)
+	
+	checksum=rtoken
+	byte[] rawBytes = [0x21, 0x31, 0x00, 0x00, 
+					   0x00, 0x00, 0x00, 0x00] 
+	String stringBytes = hubitat.helper.HexUtils.byteArrayToHexString(rawBytes)
+	stringBytes = stringBytes + rdeviceID + stampstr + checksum + msgstr
+	
+	length=stringBytes.length()
+	stringBytes=stringBytes.substring(0,4) + hubitat.helper.HexUtils.integerToHexString((length/2).toInteger(),2) + stringBytes.substring(8,length)
+	checksum = md5(hubitat.helper.HexUtils.hexStringToByteArray(stringBytes))
+	
+	stringBytes=stringBytes.substring(0,32)+checksum+stringBytes.substring(64,length)
+	if (logEnable) log.info stringBytes
+	
+	def myHubAction = new hubitat.device.HubAction(stringBytes, 
+                           hubitat.device.Protocol.LAN, 
+                           [type: hubitat.device.HubAction.Type.LAN_TYPE_UDPCLIENT, 
+                            destinationAddress: settings.ipAddress+":54321",
+                            encoding: hubitat.device.HubAction.Encoding.HEX_STRING,
+							callback: msgResponse
+							]) 
+	if (logEnable) log.info 'sending msg: ' + stringBytes
+	sendHubCommand(myHubAction)
+}
+
+def connect(callbackFunc) {
+	byte[] rawBytes = [0x21, 0x31, 0x00, 0x20, 
+					   0xFF, 0xFF, 0xFF, 0xFF, 
+					   0xFF, 0xFF, 0xFF, 0xFF, 
+					   0xFF, 0xFF, 0xFF, 0xFF, 
+					   
+					   0xFF, 0xFF, 0xFF, 0xFF,  
+					   0xFF, 0xFF, 0xFF, 0xFF, 
+					   0xFF, 0xFF, 0xFF, 0xFF, 
+					   0xFF, 0xFF, 0xFF, 0xFF
+					  ] 
+	String stringBytes = hubitat.helper.HexUtils.byteArrayToHexString(rawBytes)
+	def myHubAction = new hubitat.device.HubAction(stringBytes, 
+                           hubitat.device.Protocol.LAN, 
+                           [type: hubitat.device.HubAction.Type.LAN_TYPE_UDPCLIENT, 
+                            destinationAddress: settings.ipAddress+":54321",
+                            encoding: hubitat.device.HubAction.Encoding.HEX_STRING, 
+							callback: callbackFunc]) 
+											   
+	if (logEnable) log.info 'sending hello' 
+	sendHubCommand(myHubAction)
+}
+
+def aesEncrypt(val) {
+	return aes("encrypt", val)
+}
+def aesDecrypt(val) {
+	return aes("decrypt", val)
+}
+def getKey() {
+	return md5(hubitat.helper.HexUtils.hexStringToByteArray(state.token))
+}
+def getIV(key) {
+	return md5(hubitat.helper.HexUtils.hexStringToByteArray(key + state.token))
+}
+def aes(mode, val) {
+	try {
+		key = getKey()
+		
+		iv = getIV(key)
+		if (logEnable) log.info 'token: '+state.token + ' key: ' + key + ' iv: ' + iv
+		
+		def url = "http://" + settings.aesIP + "/cgi-bin/aes.cgi?mode=" + mode + "&key=" + key + "&iv=" + iv + "&val=" + java.net.URLEncoder.encode(val, "UTF-8") 
+		
+		if (logEnable) log.debug url
+		x=""
+
+        httpGet(["uri":url]) { resp ->
+		    if (resp.success) {
+				if (logEnable) log.debug 'result from rpi: ' +resp.data
+				x = resp.data.toString()
+				if (mode=='encrypt')
+					x = x.replace("\r","").replace("\n","")
+			}
+		}
+		return x
+    } catch (Exception ex) {
+        log.error ex.message
+		return ""
+    }
+} 
+def md5(s){
+	MessageDigest.getInstance("MD5").digest(s).encodeHex().toString()
+}
